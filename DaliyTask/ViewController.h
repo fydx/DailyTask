4350ece435f34f004f83f6b2fc1ac6951e8f8cbe
fydx <lbgg918@gmail.com>
2014-12-09 11:26:03 +0800

    Initial Commit

---
+++ DaliyTask/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DaliyTask
+//
+//  Created by LiBin on 14/12/9.
+//  Copyright (c) 2014年 LiBin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
